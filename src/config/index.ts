2c7e3047498284add9953e19688d193ee664c506
shivbilgundi11 <shivbbilgundi11@gmail.com>
2026-06-16 18:19:37 +0530

    feat: configured CORS

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,6 +4,8 @@ dotenv.config();
 
 const config = {
   PORT: process.env.PORT || 3000,
+  NODE_ENV: process.env.NODE_ENV,
+  WHITELISTED_ORIGINS: ['https://docs.blog-api.codewithshiv.com'],
 };
 
 export default config;
--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,40 @@
 
 // Node modules
 import express from 'express';
+import cors from 'cors';
 
 // Custom modules
 import config from '@/config/index';
 
+// Types
+import type { CorsOptions } from 'cors';
+
 // Express app initialization
 const app = express();
+
+// configure cors options
+const corsOptions: CorsOptions = {
+  origin(origin, callback) {
+    if (
+      config.NODE_ENV === 'development' ||
+      !origin ||
+      config.WHITELISTED_ORIGINS.includes(origin)
+    ) {
+      callback(null, true);
+    } else {
+      // Reject requests from non-whitelisted origins
+      callback(
+        new Error(`CORS error: ${origin} is not allowed by CORS policy`),
+        false,
+      );
+
+      console.log(`CORS error: ${origin} is not allowed by CORS policy`);
+    }
+  },
+};
+
+// Middleware setup
+app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello, World!' });
