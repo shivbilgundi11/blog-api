053b236941dcee214053374c6281ddb653230906
shivbilgundi11 <shivbbilgundi11@gmail.com>
2026-06-21 13:07:54 +0530

    api: register user api integrated successfully!

--- src/routes/v1/auth.ts
+++ src/routes/v1/auth.ts
@@ -1,5 +1,6 @@
 // Node modules
 import { Router } from 'express';
+import { body } from 'express-validator';
 
 /**
  * Controllers
@@ -9,13 +10,45 @@ import register from '@/controllers/v1/auth/register';
 /**
  * Middlewares
  */
+import ValidationError from '@/middlewares/validationError';
 
 /**
  * Models
  */
+import User from '@/models/user';
 
 const router = Router();
 
-router.post('/register', register);
+router.post(
+  '/register',
+  body('email')
+    .trim()
+    .notEmpty()
+    .withMessage('Email is required')
+    .isLength({ max: 50 })
+    .withMessage('Email must be less than 50')
+    .isEmail()
+    .withMessage('Invalid email address')
+    .custom(async (value) => {
+      const userExists = await User.exists({ email: value });
+
+      if (userExists) {
+        throw new Error('User email or password is invalid');
+      }
+    }),
+  body('password')
+    .notEmpty()
+    .withMessage('Password is required')
+    .isLength({ min: 8 })
+    .withMessage('Password must be atleast 8 characters long'),
+  body('role')
+    .optional()
+    .isString()
+    .withMessage('Role must be a string')
+    .isIn(['admin', 'user'])
+    .withMessage('Role must be either admin or user'),
+  ValidationError,
+  register,
+);
 
 export default router;
