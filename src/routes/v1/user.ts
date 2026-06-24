5497cb6bf93e4cbf1ff50b70a46fe71f5e74dd55
shivbilgundi11 <shivbbilgundi11@gmail.com>
2026-06-24 23:32:23 +0530

    api: integrated get user by id API

--- src/routes/v1/user.ts
+++ src/routes/v1/user.ts
@@ -18,6 +18,7 @@ import getCurrentUser from '@/controllers/v1/user/get_current_user';
 import updateCurrentUser from '@/controllers/v1/user/update_current_user';
 import deleteCurrentUser from '@/controllers/v1/user/delete_curr_user';
 import getAllUsers from '@/controllers/v1/user/get_all_users';
+import getUserById from '@/controllers/v1/user/get_user_by_id';
 
 /**
  * Models
@@ -105,6 +106,15 @@ router.get(
     .withMessage('Offset must be a positive integer'),
   validationError,
   getAllUsers,
+);
+
+router.get(
+  '/:userId',
+  authenticate,
+  authorize(['admin']),
+  param('userId').notEmpty().isMongoId().withMessage('Invalid user id'),
+  validationError,
+  getUserById,
 );
 
 export default router;
